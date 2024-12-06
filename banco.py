501a13d7440368a380179f2d592bdabb2d5b4dbd
RodrigoPiresgames <91659724+RodrigoPiresgames@users.noreply.github.com>
2024-12-06 12:21:12 +0000

    Add files via upload

--- banco.py
+++ banco.py
@@ -11,11 +11,12 @@ class ContaBancaria:
     def depositar(self, to_deposit):     
         if(to_deposit < 0):
             print("0")
-        self.saldo += to_deposit
-        print("1")
+        else:
+            self.saldo += to_deposit
+            print("1")
         
     def levantar(self, to_extract):
-        if(self.saldo + self.credit <= to_extract):
+        if(self.saldo + self.credit >= to_extract):
             self.saldo += -to_extract
             print("1")
         else:
@@ -25,7 +26,7 @@ class ContaBancaria:
         print("{:.2f}".format(self.saldo))
     
     def exibir_info(self):
-        print("[{self.titular}] [{self.saldo}] [{self.credit}]")
+        print(f"{self.titular} {self.saldo} {self.credit}")
 
 conta = ContaBancaria("João", 1000.00, 500.00)
 conta.depositar(-500.00)
